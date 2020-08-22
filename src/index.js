0361634925e744644358cb2171471eee1b762061
sitiojcb <info@webjcb.com>
2020-08-22 16:22:15 -0300

    update menu-item.component withRouter HOC

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// using ES6 modules
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 
 
 ReactDOM.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
