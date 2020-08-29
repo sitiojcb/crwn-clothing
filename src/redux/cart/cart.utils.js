8f38f401c9fd9586be2b0fc5d1d771856fb92e59
sitiojcb <info@webjcb.com>
2020-08-29 11:41:15 -0300

    created cart-item component and connected to cart-dropdown

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,12 +1,14 @@
 export const addItemToCart = (cartItems , cartItemToAdd) => {
-    const existingCartItem = cartItem.find(cartItem => cartItem.id === cartItemToAdd.id )
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id )
+
+    if (existingCartItem) {
+        return cartItems.map(cartItem =>
+            cartItem.id === cartItemToAdd.id ?
+                { ...cartItem, quantity: cartItem.quantity + 1 } :
+                cartItem
+        )
+    }
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
 
-if(existingCartItem) {
-    return cartItem.map(cartItem => 
-        cartItem.id === cartItemToAdd.id ?
-        {...cartItem, quantity: cartItem.quantity + 1} :
-        cartItem 
-        )
-}
-return [...cartItems, {...cartItemToAdd, quantity: 1}]
+
